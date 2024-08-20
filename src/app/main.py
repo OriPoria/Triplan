dac85eae11bcce1bdc5edd8af46fec211ddfb0a0
ori <ori_poria_tp@bmc.com>
2024-08-20 21:14:46 +0300

    get trip from ai prompt

--- src/app/main.py
+++ src/app/main.py
@@ -1,19 +1,9 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI
 
-from src.models.user import User
 from src.models.trip import Trip
-from src.db.users_db_handler import UsersDbHandler
 
 app = FastAPI()
 
 
-users_db = UsersDbHandler()
-
-@app.post("/users")
-async def create_user(user: User) -> str:
-    users_db.create_user(user)
-    return "New user added successfully to Triplan systems"
-
-
-async def create_trip(trip_spec: Trip):
+async def get_trip(input_prompt) -> Trip:
     pass
